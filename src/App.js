878277508c87f95d027eff6d0857fb909621d592
Nomisnoml <hoehn_maurice@gmx.de>
2024-09-10 17:15:41 +0200

    redrawStack

--- src/App.js
+++ src/App.js
@@ -29,69 +29,15 @@ class GameClient {
 
 
   update(state) {
-    let allEmpty = true;
-    for(let i = 0; i < state.G.length; i++){
-      if(state.G[i] != null){
-        allEmpty = false;
-      }
-    }
-    if(allEmpty){
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      this.createBoard()
-    }
-    ctx.fillStyle = "black";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    ctx.font = "20px serif";
-    let id = 0;
-    for(let i = 0; i < 3; i++){
-      for(let b = 0; b < 3; b++){
-        if(state.G.cells[id] == null){
-          ctx.fillText(" ", 100+b*50, 100+i*50, 50)
-        } else{
-          ctx.fillText(state.G.cells[id], 100+b*50, 100+i*50, 50)
-        }
-        id++;
-      }
-    }
 
-
-    if (state.ctx.gameover) {
-      if (state.ctx.gameover.winner !== undefined) {
-        setTimeout(() => alert("Winner: " + String(state.ctx.gameover.winner)), 0);
-      } else {
-        setTimeout(() => alert("Draw!"), 0);
-      }
-    }
   }
 
   createBoard() {
-    ctx.fillStyle = 'black'
-    ctx.fillRect(50, 50, 200, 200);
-    let change = true;
-    for(let i = 0; i < 3; i++){
-      for(let b = 0; b < 3; b++){
-        if(change){
-          ctx.fillStyle = '#F5F5DC'
-        } else {
-          ctx.fillStyle = "#D2B48C"
-        }
-        change = !change
-        ctx.fillRect(75+i*50, 75+b*50, 50, 50);
-      }
-    }
+
   }
 
   attachListeners() {
-    onClick (75, 75, 50, 50, () => { this.client.moves.clickCell(0)} )
-    onClick (75+50, 75, 50, 50, () => { this.client.moves.clickCell(1)} )
-    onClick (75+100, 75, 50, 50, () => { this.client.moves.clickCell(2)} )
-    onClick (75, 75+50, 50, 50, () => { this.client.moves.clickCell(3)} )
-    onClick (75+50, 75+50, 50, 50, () => { this.client.moves.clickCell(4)} )
-    onClick (75+100, 75+50, 50, 50, () => { this.client.moves.clickCell(5)} )
-    onClick (75, 75+100, 50, 50, () => { this.client.moves.clickCell(6)} )
-    onClick (75+50, 75+100, 50, 50, () => { this.client.moves.clickCell(7)} )
-    onClick (75+100, 75+100, 50, 50, () => { this.client.moves.clickCell(8)} )
+
   }
 }
 
--- src/theMIND.js
+++ src/theMIND.js
@@ -2,16 +2,11 @@ import { INVALID_MOVE } from 'boardgame.io/core';
 
 export const TicTacToe = {
   setup: function setup() {
-    return { cells: [null, null, null, null, null, null, null, null, null]}
+    console.log(initialRedrawStack())
+    return {}
   },
 
   moves: {
-    clickCell: function clickCell(move, cellIndex) {
-      if (move.G.cells[cellIndex] !== null) {
-        return INVALID_MOVE;
-      }
-      move.G.cells[cellIndex] = move.playerID;
-    },
 
   },
 
@@ -22,55 +17,37 @@ export const TicTacToe = {
   },
 
   endIf: function endIf(endIf) {
-    const winner = isVictory(endIf.G.cells);
-    if (winner != null) {
-      return { winner: winner };
-    }
-    if (isDraw(endIf.G.cells)) {
-      return { draw: true };
-    }
+
   },
 
   ai: {
-    enumerate: function enumerate(G)  {
-      let possibleMoves = []
-      for(let i = 0; i < G.cells.length; i++){
-        if(G[i] == null){
-          possibleMoves.push({move: 'clickCell', args: [i]})
-        }
-      }
-      console.log(possibleMoves)
-      return possibleMoves
-    },
+
   },
 
 }
-
-function isVictory(cells){
-  for(let i = 0; i < 3; i++ ){
-    if(cells[3*i] == cells[i*3+1] && cells[i*3+1] == cells[i*3+2] && cells[3*i] != null){
-      return cells[i*3]
-    }
+function initialRedrawStack() {
+  let array = []
+  for (let i=1; i<101; i++) {
+    array.push(i)
   }
-  for(let i = 0; i < 3; i++ ){
-    if(cells[i] == cells[i+3] && cells[i+3] == cells[i+6] && cells[i] != null){
-      return cells[i]
-    }
-  }
-  if((cells[2] == cells[4] && cells[4] == cells[6])||(cells[0] == cells[4] && cells[4] == cells[8]) && cells[4] != null) {
-    return cells[4]
-  }
-
-  return null
-  
-
+  return (shuffle(array))
 }
 
-function isDraw(cells) {
-  for(let i = 0; i < cells.length; i++){
-    if(cells[i] == null){
-      return false;
-    }
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
-  return true;
+  return (array)
 }
+
